d1694d0c2d1722e5d3ea34c3620e2d11c4fa253c
siddhantpurohit216 <siddhantpurohit216@gmail.com>
2024-09-13 22:49:51 +0530

    second commit

--- promiseApi.js
+++ promiseApi.js
@@ -1,25 +1,33 @@
 const p1= new Promise((res,rej)=>{
     setTimeout(()=>{
-        res("Helloo from promise 1")
-        // rej("Promise 2 failed!")
+        // res("Helloo from promise 1")
+        rej("Promise 2 failed!")
 
     },2000)
 })
 const p2= new Promise((res,rej)=>{
     setTimeout(()=>{
-        // res("Helloo from promise 2")
-        rej("Promise 2 failed!")
+        res("Helloo from promise 2")
+        // rej("Promise 2 failed!")
     },3000)
 })
 const p3= new Promise((res,rej)=>{
     setTimeout(()=>{
-        // res("Helloo from promise 3")
-        rej("Promise 2 failed!")
+        res("Helloo from promise 3")
+        // rej("Promise 2 failed!")
 
     },4000)
 })
 
-const result= Promise.all([p1,p2,p3]);
+// const result= Promise.all([p1,p2,p3]);
+// result.then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.error(err)
+// })
+
+
+const result= Promise.any([p1,p2,p3]);
 result.then((data)=>{
     console.log(data)
 }).catch((err)=>{
